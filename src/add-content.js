34d0b67a0b6726638414f3769a92a40a67245331
孙华鹏 <sunhuapeng@heydayscape.com>
2020-10-13 19:39:17 +0800

    配置本地dev-server

--- src/add-content.js
+++ src/add-content.js
@@ -2,8 +2,8 @@
  * @Author: sunhuapeng
  * @Date: 2020-10-13 18:35:18
  * @LastEditors: sunhuapeng
- * @LastEditTime: 2020-10-13 19:07:42
+ * @LastEditTime: 2020-10-13 19:28:33
  */
 export default function (){
-  document.write('I\' m using a config file!')
+  document.write('I\' m using webpack-dev-server!')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@
  * @Author: sunhuapeng
  * @Date: 2020-10-13 19:02:20
  * @LastEditors: sunhuapeng
- * @LastEditTime: 2020-10-13 19:07:09
+ * @LastEditTime: 2020-10-13 19:33:24
  */
 module.exports = {
   entry: './src/index.js', // 资源入口
   output: { // 更多详细配置的对象
-    filename: 'build.js' // 
-  }   
+    filename: './build.js' // 
+  },
+  devServer: {
+    publicPath: '/dist'
+  }
 }
