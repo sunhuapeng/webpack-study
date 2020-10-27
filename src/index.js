267d938fd2e403ac9dac1bf05636ae61f4448fe3
孙华鹏 <sunhuapeng@heydayscape.com>
2020-10-27 15:14:11 +0800

    loader的应用

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@
  * @LastEditTime: 2020-10-13 18:35:12
  */
 import addContent from './add-content.js'
+
+import './style.css'
 document.write('My first Webpach app. <br />')
 addContent()
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,24 @@
  * @LastEditors: sunhuapeng
  * @LastEditTime: 2020-10-13 19:33:24
  */
-const path = require('path')
+const path = require("path");
 module.exports = {
-  entry: './src/index.js', // 资源入口
-  output: { // 更多详细配置的对象
-    filename: 'static/js/[name] @ [chunkhash].js', // [name] [chunkhash] 与chunkhash有关 可以控制客户端缓存  [name] @ [chunkhash].js
-    path: path.join(__dirname, 'dist')  //webpacl4.+默认dist目录，除非我们需要改变它否则不必单独配置
+  entry: "./src/index.js", // 资源入口
+  output: {
+    // 更多详细配置的对象
+    filename: "bundle.js", // [name] [chunkhash] 与chunkhash有关 可以控制客户端缓存  [name] @ [chunkhash].js
+    path: path.join(__dirname, "dist"), //webpacl4.+默认dist目录，除非我们需要改变它否则不必单独配置
   },
   devServer: {
-    publicPath: path.join(__dirname, 'dist'),
-  }
-}
+    publicPath: "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
+      },
+    ],
+  },
+};
